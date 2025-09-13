3f2f4561f070eae7dbafb9e3a6610ba148269185
sunbrolynk <swagar003@gmail.com>
2025-09-13 15:05:01 -0600

    fucked up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import stats
+from stats import get_book_text, get_num_words, char_count, sorted_list
 
-word_count = get_numm_words("books/frankenstein.txt")
+word_count = get_num_words("books/frankenstein.txt")
 char_dict = char_count("books/frankenstein.txt")
 sorted_chars = sorted_list(char_dict)
 
