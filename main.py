8d6d9459529702354d9d0d95400f187bd01445ed
sunbrolynk <swagar003@gmail.com>
2025-09-13 15:00:08 -0600

    almost done

---
+++ main.py
@@ -0,0 +1,21 @@
+from stats import get_book_text, get_num_words, char_count, sorted_list
+
+word_count = get_numm_words("books/frankenstein.txt")
+char_dict = char_count("books/frankenstein.txt")
+sorted_chars = sorted_list(char_dict)
+
+
+
+
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+
+print(f"Found {word_count} total words")
+
+print("--------- Character Count -------")
+
+
+
+print("============= END ===============")
---
+++ stats.py
@@ -0,0 +1,45 @@
+def get_book_text(filepath):
+	file_contents = ""
+	with open(filepath) as f:
+		file_contents += f.read()
+	return file_contents
+
+
+
+def get_num_words(filepath):
+	book_string = get_book_text(filepath)
+	split_strings = book_string.split()
+	word_count = 0
+	for string in split_strings:
+		word_count += 1
+	return word_count
+
+
+def char_count(filepath):
+	book_string = get_book_text(filepath)
+	lower_b_string = book_string.lower()
+	char_count = {}
+	for char in lower_b_string:
+		if char not in char_count:
+			char_count[char] = 1
+		elif char in char_count:
+			char_count[char] += 1
+
+	return char_count
+
+
+
+def sort_on(items):
+	return items["num"]
+
+
+
+def sorted_list(char_count):
+	sorted_list = []
+	for key,value in char_count.items():
+		sorted_list.append({"char": key,"num": value})
+
+	sorted_list.sort(reverse=True, key=sort_on)
+
+
+	return sorted_list
