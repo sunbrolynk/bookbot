2e805ce74b5c82dc5c9490d625f3271f8337bf30
sunbrolynk <swagar003@gmail.com>
2025-09-14 17:51:28 -0600

    working

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from stats import *
 import sys
 
+if len(sys.argv) != 2:
+	print("Usage: python main.py <path-to-book>")
+	sys.exit(1)
+
 word_count = get_num_words(sys.argv[1])
 char_dict = char_count(sys.argv[1])
 sorted_chars = sorted_list(char_dict)
 
 
-if len(sys.argv) != 2:
-	print("Usage: python main.py <path-to-book>")
-	sys.exit(1)
+
 
 print("============ BOOKBOT ============")
 print(f"Analyzing book found at {sys.argv[1]}...")
